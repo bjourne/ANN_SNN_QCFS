be7ffdd7014e1799e32634c942d4a8ac9c750298
Björn Lindqvist <bjourne@gmail.com>
2024-09-28 18:16:55 +0200

    Stylistic changes

--- main_test.py
+++ main_test.py
@@ -3,6 +3,7 @@ import os
 import torch
 import warnings
 
+from argparse import ArgumentParser
 from models import modelpool
 from os import environ
 from pathlib import Path
@@ -10,22 +11,31 @@ from preprocess import datapool
 from utils import train, val, seed_all, get_logger
 from models.layer import *
 
-parser = argparse.ArgumentParser(description='PyTorch Training')
-# just use default setting
-parser.add_argument(
-    '-j', '--workers',
-    default = 4,
-    type = int,
-    metavar = 'N',
-    help = 'number of data loading workers'
-)
-parser.add_argument(
-    '-b','--batch_size',default=200, type=int,metavar='N',help='mini-batch size'
-)
-parser.add_argument('--seed',default=42,type=int,help='seed for initializing training. ')
-parser.add_argument('-suffix','--suffix',default='', type=str,help='suffix')
-
 def main():
+    parser = ArgumentParser(description='PyTorch Training')
+    parser.add_argument(
+        'weights_file',
+        help = 'path to network weights file'
+    )
+    parser.add_argument(
+        '-j', '--workers',
+        default = 4,
+        type = int,
+        metavar = 'N',
+        help = 'number of data loading workers'
+    )
+    parser.add_argument(
+    '-b','--batch_size',default=200,
+        type=int,metavar='N',help='mini-batch size'
+    )
+    parser.add_argument(
+        '--seed',default=42,
+        type=int,help='seed for initializing training.'
+    )
+    parser.add_argument(
+        '-suffix','--suffix',
+        default='', type=str,help='suffix'
+    )
     parser.add_argument(
         '-data', '--dataset',
         default='cifar100',type=str,help='dataset'
@@ -33,10 +43,6 @@ def main():
     parser.add_argument(
         '-arch','--model',default='vgg16',
         type=str,help='network'
-    )
-    parser.add_argument(
-        '-id', '--identifier',
-        type=str,help='model statedict identifier'
     )
     parser.add_argument(
         '-dev','--device',default='0',type=str,help='device'
@@ -53,31 +59,13 @@ def main():
 
     _, l_te = datapool(args.dataset, args.batch_size)
     net = modelpool(args.model, args.dataset)
-
-    dir = Path('%s-checkpoints' % args.dataset)
-    path = dir / (args.identifier + '.pth')
-    state_dict = torch.load(path, map_location=torch.device('cpu'))
-
-    # # if old version state_dict
-    # keys = list(state_dict.keys())
-    # print('keys', keys)
-    # for k in keys:
-    #     if "relu.up" in k:
-    #         print('here!')
-    #         state_dict[k[:-7]+'act.thresh'] = state_dict.pop(k)
-    #     elif "up" in k:
-    #         print('here!')
-    #         state_dict[k[:-2]+'thresh'] = state_dict.pop(k)
+    state_dict = torch.load(args.weights_file, map_location=torch.device('cpu'))
 
     net.load_state_dict(state_dict)
 
     net = net.to(dev)
     net.set_T(args.time)
     net.set_L(8)
-
-    for m in net.modules():
-        if isinstance(m, IF):
-            print(m.thresh)
 
     acc = val(net, l_te, dev, args.time)
     print(acc)
--- main_train.py
+++ main_train.py
@@ -1,10 +1,4 @@
-import argparse
-import torch
-import warnings
-import torch.nn as nn
-import torch.nn.parallel
-import torch.optim
-
+from argparse import ArgumentParser
 from models import modelpool
 from os import environ
 from preprocess import datapool
@@ -12,37 +6,78 @@ from torch.optim import SGD
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from utils import train, val, seed_all
 
-parser = argparse.ArgumentParser(description='PyTorch Training')
-# just use default setting
-
-# 4 workers ~ 2s/batch
-
-parser.add_argument('-j','--workers', default=4, type=int,metavar='N',help='number of data loading workers')
-parser.add_argument('-b','--batch_size', default=300, type=int,metavar='N',help='mini-batch size')
-parser.add_argument('--seed', default=42, type=int, help='seed for initializing training. ')
-parser.add_argument('-suffix','--suffix', default='', type=str,help='suffix')
-parser.add_argument('-T', '--time', default=0, type=int, help='snn simulation time')
-
-# model configuration
-parser.add_argument('-data', '--dataset',default='cifar100',type=str,help='dataset')
-parser.add_argument('-arch','--net',default='vgg16',type=str,help='net')
-
-# training configuration
-parser.add_argument('--epochs',default=300,type=int,metavar='N',help='number of total epochs to run')
-# 0.05 for cifar100 / 0.1 for cifar10
-parser.add_argument('-lr','--lr',default=0.1,type=float,metavar='LR', help='initial learning rate')
-parser.add_argument('-wd','--weight_decay',default=5e-4, type=float, help='weight_decay')
-parser.add_argument('-dev','--device',default='0',type=str,help='device')
-parser.add_argument('-L', '--L', default=8, type=int, help='Step L')
-
-args = parser.parse_args()
-
-environ["CUDA_VISIBLE_DEVICES"] = args.device
-dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import torch
 
 def main():
-    global args
+    parser = ArgumentParser(description='PyTorch Training')
+    parser.add_argument(
+        '-j','--workers',
+        default=4, type=int,metavar='N',
+        help='number of data loading workers'
+    )
+    parser.add_argument(
+        '-b','--batch_size',
+        default=300, type=int,metavar='N',
+        help='mini-batch size'
+    )
+    parser.add_argument(
+        '--seed',
+        default=42, type=int,
+        help='seed for initializing training'
+    )
+    parser.add_argument(
+        '-suffix','--suffix', default='',
+        type=str,help='suffix'
+    )
+    parser.add_argument(
+        '-T', '--time', default=0, type=int,
+        help='snn simulation time'
+    )
+
+    # model configuration
+    parser.add_argument(
+        '-data', '--dataset',
+        default='cifar100', type=str,
+        help='dataset'
+    )
+    parser.add_argument(
+        '-arch','--net',
+        default='vgg16',type=str,
+        help='net'
+    )
+
+    parser.add_argument(
+        '--epochs',
+        default=300,type=int,metavar='N',
+        help='number of total epochs to run'
+    )
+    # 0.05 for cifar100 / 0.1 for cifar10
+    parser.add_argument(
+        '-lr', '--lr',
+        default = 0.1, type = float,
+        metavar = 'LR', help = 'initial learning rate'
+    )
+    parser.add_argument(
+        '-wd','--weight_decay',
+        default=5e-4, type=float,
+        help='weight_decay'
+    )
+    parser.add_argument(
+        '-dev','--device',
+        default='0',type=str,help='device'
+    )
+    parser.add_argument(
+        '-L', '--L',
+        default=8, type=int, help='Step L'
+    )
+    args = parser.parse_args()
+
+    environ["CUDA_VISIBLE_DEVICES"] = args.device
+    dev = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     seed_all(args.seed)
+
+    print('batch size', args.batch_size)
+
     # preparing data
     l_tr, l_te = datapool(args.dataset, args.batch_size)
     # preparing net
@@ -51,7 +86,12 @@ def main():
 
     net.to(dev)
 
-    opt = SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=args.weight_decay)
+    opt = SGD(
+        net.parameters(),
+        lr=args.lr,
+        momentum=0.9,
+        weight_decay=args.weight_decay
+    )
     scheduler = CosineAnnealingLR(opt, T_max=args.epochs)
     best_acc = 0
 
@@ -69,16 +109,13 @@ def main():
         tr_loss, tr_acc = train(net, dev, l_tr, opt, epoch, n_epochs)
         scheduler.step()
 
-        print(scheduler.get_last_lr())
-
         net.eval()
         te_loss, te_acc = train(net, dev, l_te, opt, epoch, n_epochs)
 
-        fmt = 'losses %5.3f/%5.3f acc %5.3f/%5.3f'
-        print(fmt % (tr_loss, te_loss, tr_acc, te_acc))
+        fmt = 'losses %5.3f/%5.3f acc %5.3f/%5.3f (best: %5.3f)'
+        print(fmt % (tr_loss, te_loss, tr_acc, te_acc, best_acc))
 
         if best_acc < te_acc:
-            print('New best accuracy: %5.3f' % te_acc)
             best_acc = te_acc
             torch.save(net.state_dict(), '%s.pth' % identifier)
 
