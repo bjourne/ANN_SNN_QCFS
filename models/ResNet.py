18aa2a5fad94c777293a297013f5187ecb51a273
Björn Lindqvist <bjourne@gmail.com>
2024-10-02 20:43:32 +0200

    Simplifying stuff

--- models/ResNet.py
+++ models/ResNet.py
@@ -13,12 +13,6 @@ import torch.nn as nn
 class BasicBlock(Module):
     """Basic Block for resnet 18 and resnet 34
     """
-    #BasicBlock and BottleNeck block
-    #have different output size
-    #we use class attribute expansion
-    #to distinct
-    expansion = 1
-
     def __init__(self, n_in, n_out, stride, T, L):
         super().__init__()
         self.residual_function = Sequential(
@@ -29,18 +23,18 @@ class BasicBlock(Module):
             BatchNorm2d(n_out),
             IF(T, L),
             Conv2d(
-                n_out, n_out * BasicBlock.expansion,
+                n_out, n_out,
                 kernel_size=3, padding=1, bias=False
             ),
-            BatchNorm2d(n_out * BasicBlock.expansion)
+            BatchNorm2d(n_out)
         )
         self.shortcut = Sequential()
         #the shortcut output dimension is not the same with residual function
         #use 1*1 convolution to match the dimension
-        if stride != 1 or n_in != BasicBlock.expansion * n_out:
+        if stride != 1 or n_in != n_out:
             self.shortcut = nn.Sequential(
-                Conv2d(n_in, n_out * BasicBlock.expansion, kernel_size=1, stride=stride, bias=False),
-                BatchNorm2d(n_out * BasicBlock.expansion)
+                Conv2d(n_in, n_out, kernel_size=1, stride=stride, bias=False),
+                BatchNorm2d(n_out)
             )
         self.act = IF(T, L)
 
@@ -48,39 +42,32 @@ class BasicBlock(Module):
         x = self.residual_function(x) + self.shortcut(x)
         return self.act(x)
 
+def make_layer(n_in, n_out, n_blocks, stride, T, L):
+    strides = [stride] + [1] * (n_blocks - 1)
+    layers = []
+    for stride in strides:
+        layers.append(BasicBlock(n_in, n_out, stride, T, L))
+        n_in = n_out
+    return Sequential(*layers)
+
 class ResNet(Module):
-    def __init__(self, block, num_block, n_classes, T, L):
+    def __init__(self, num_block, n_classes, T, L):
         super().__init__()
-        self.in_channels = 64
         self.T = T
-        # self.merge = MergeTemporalDim(0)
-        # self.expand = ExpandTemporalDim(0)
         self.conv1 = Sequential(
             Conv2d(3, 64, kernel_size=3, padding=1, bias=False),
             BatchNorm2d(64),
-            IF()
+            IF(T, L)
         )
         # we use a different inputsize than the original paper
         # so conv2_x's stride is 1
-        self.conv2_x = self._make_layer(block, 64, num_block[0], 1, T, L)
-        self.conv3_x = self._make_layer(block, 128, num_block[1], 2, T, L)
-        self.conv4_x = self._make_layer(block, 256, num_block[2], 2, T, L)
-        self.conv5_x = self._make_layer(block, 512, num_block[3], 2, T, L)
-        self.avg_pool = AdaptiveAvgPool2d((1, 1))
-        self.fc = Linear(512 * block.expansion, n_classes)
+        self.conv2_x = make_layer(64, 64, num_block[0], 1, T, L)
+        self.conv3_x = make_layer(64, 128, num_block[1], 2, T, L)
+        self.conv4_x = make_layer(128, 256, num_block[2], 2, T, L)
+        self.conv5_x = make_layer(256, 512, num_block[3], 2, T, L)
 
-    def _make_layer(self, block, out_channels, num_blocks, stride, T, L):
-        # we have num_block blocks per layer, the first block
-        # could be 1 or 2, other blocks would always be 1
-        strides = [stride] + [1] * (num_blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(
-                self.in_channels, out_channels, stride,
-                T, L
-            ))
-            self.in_channels = out_channels * block.expansion
-        return Sequential(*layers)
+        self.avg_pool = AdaptiveAvgPool2d((1, 1))
+        self.fc = Linear(512, n_classes)
 
     def forward(self, x):
         if self.T > 0:
@@ -102,7 +89,7 @@ class ResNet(Module):
         return x
 
 class ResNet4Cifar(nn.Module):
-    def __init__(self, block, num_block, n_classes=10):
+    def __init__(self, num_block, n_classes=10):
         super().__init__()
         self.in_channels = 16
         self.T = 0
@@ -114,18 +101,18 @@ class ResNet4Cifar(nn.Module):
             IF())
         # we use a different inputsize than the original paper
         # so conv2_x's stride is 1
-        self.conv2_x = self._make_layer(block, 16, num_block[0], 1)
-        self.conv3_x = self._make_layer(block, 32, num_block[1], 2)
-        self.conv4_x = self._make_layer(block, 64, num_block[2], 2)
+        self.conv2_x = self._make_layer(16, num_block[0], 1)
+        self.conv3_x = self._make_layer(32, num_block[1], 2)
+        self.conv4_x = self._make_layer(64, num_block[2], 2)
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(64 * block.expansion, n_classes)
+        self.fc = nn.Linear(64, n_classes)
 
-    def _make_layer(self, block, out_channels, num_blocks, stride):
+    def _make_layer(self, n_out, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_channels, out_channels, stride))
-            self.in_channels = out_channels * block.expansion
+            layers.append(BasicBlock(self.in_channels, n_out, stride))
+            self.in_channels = n_out
         return nn.Sequential(*layers)
 
     def set_T(self, T):
@@ -159,10 +146,10 @@ class ResNet4Cifar(nn.Module):
         return output
 
 def resnet18(n_classes, T, L):
-    return ResNet(BasicBlock, [2, 2, 2, 2], n_classes, T, L)
+    return ResNet([2, 2, 2, 2], n_classes, T, L)
 
 def resnet20(n_classes=10):
-    return ResNet4Cifar(BasicBlock, [3, 3, 3], n_classes=n_classes)
+    return ResNet4Cifar([3, 3, 3], n_classes=n_classes)
 
 def resnet34(n_classes=10):
-    return ResNet(BasicBlock, [3, 4, 6, 3], n_classes=n_classes)
+    return ResNet([3, 4, 6, 3], n_classes=n_classes)
